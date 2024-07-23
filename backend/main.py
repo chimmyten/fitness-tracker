e7877223e9448ab68c124daf635433129570e115
Timmy Chen <chenx343@gmail.com>
2024-07-22 21:57:35 -0400

    add delete route and switch port

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+from bson.objectid import ObjectId
 
 import os
 from dotenv import load_dotenv, dotenv_values
@@ -28,10 +29,16 @@ def get_workouts():
 def add_workout(): 
   new_workout = request.json
   result = mongo.db.workouts.insert_one(new_workout)
-  if (result.acknowledged == True):
+  return (f"Workout {result.inserted_id} inserted!")
+  
+
+@app.route("/<id>", methods=["DELETE"])
+def delete_workout(id):
+  result = mongo.db.workouts.delete_one({"_id": ObjectId(id)})
+  if (result.deleted_count == 1):
     return "Success!"
   else:
     return "Error!"
-  
+
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(port = 8000, debug=True)
