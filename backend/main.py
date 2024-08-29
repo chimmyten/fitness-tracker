887b4753e950c049d01b0715bac2aa9324720ed3
Timmy Chen <chenx343@gmail.com>
2024-08-29 12:35:59 -0400

    configure for mongodb atlas

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ app.config['MONGO_URI'] = os.getenv("MONGO_DB_URI")
 app.config['SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 
 mongo = PyMongo(app)
+db = mongo.cx['fitness-tracker-db']
 
 def mongo_to_json(doc):
   doc['_id'] = str(doc['_id'])
@@ -35,7 +36,7 @@ def token_required(f):
     
     try:
       data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-      current_user = mongo.db.users.find_one({'_id': ObjectId(data['userId'])})
+      current_user = db['users'].find_one({'_id': ObjectId(data['userId'])})
 
     except Exception as e:
       return jsonify({"message": "Token missing or expired"}), 401
@@ -49,7 +50,7 @@ def token_required(f):
 def get_workouts(current_user):
   type = request.args.get("type")
   print(f"TYPE: {type}")
-  workouts = mongo.db.workouts.find({"user_id": current_user['_id'], "type": type})
+  workouts = db['workouts'].find({"user_id": current_user['_id'], "type": type})
   workouts_list = [mongo_to_json(workout) for workout in workouts]
   return jsonify(workouts_list)
 
@@ -58,14 +59,14 @@ def get_workouts(current_user):
 def add_workout(current_user): 
   new_workout = request.json
   new_workout['user_id'] = current_user['_id']
-  result = mongo.db.workouts.insert_one(new_workout)
+  result = db['workouts'].insert_one(new_workout)
   return (f"Workout {result.inserted_id} inserted!")
   
 
 @app.route("/<id>", methods=["DELETE"])
 @token_required
 def delete_workout(current_user, id):
-  result = mongo.db.workouts.delete_one({"user_id": current_user['_id'], "_id": ObjectId(id)})
+  result = db['workouts'].delete_one({"user_id": current_user['_id'], "_id": ObjectId(id)})
   if (result.deleted_count == 1):
     return "Success!"
   else:
@@ -79,7 +80,7 @@ def update_workout(current_user, id):
   if ('_id' in updated_workout):
     del updated_workout['_id']
 
-  result = mongo.db.workouts.update_one({"user_id": current_user["_id"], '_id': ObjectId(id)}, {'$set': updated_workout})
+  result = db['workouts'].update_one({"user_id": current_user["_id"], '_id': ObjectId(id)}, {'$set': updated_workout})
   if result.matched_count > 0:
     return jsonify({"message": "Workout updated successfully"}), 200
   else:
@@ -93,13 +94,13 @@ def register_user():
   username = data.get("username")
   password = data.get("password")
 
-  user = mongo.db.users.find_one({"username": username})
+  user = db['users'].find_one({"username": username})
   if user:
     return jsonify({"message": "User already exists"}), 400
   
   hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-  mongo.db.users.insert_one({"username": username, "password": hashed_password})
+  db['users'].insert_one({"username": username, "password": hashed_password})
   return jsonify({"message": "User created successfully"}), 201
 
 @app.route("/login", methods=["POST"])
@@ -108,7 +109,7 @@ def login():
   username = user_data.get("username")
   password = user_data.get("password").encode('utf-8')
 
-  user_db = mongo.db.users.find_one({"username": username})
+  user_db = db['users'].find_one({"username": username})
   
   if user_db and bcrypt.checkpw(password, user_db["password"]):
     token = jwt.encode({
